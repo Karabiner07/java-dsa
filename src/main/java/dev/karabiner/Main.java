e4b571e86fe7fd257d43f6358544ea6429637734
Karabiner07 <joyphilip.p2001@gmail.com>
2024-12-16 23:48:37 +0530

    String Palindrome

--- src/main/java/dev/karabiner/Main.java
+++ src/main/java/dev/karabiner/Main.java
@@ -1,15 +1,11 @@
 package dev.karabiner;
 
-
-import dev.karabiner.array.ArrayUtil;
+import dev.karabiner.string.StringUtil;
 
 public class Main {
     public static void main(String[] args) {
 
-        //create array with a missing number
-        int[] array = new int[] {2, 3, 1, 5, 6, 4, 9, 7, 10};
-
-        System.out.println(ArrayUtil.findMissingElement(array));
+        System.out.println(StringUtil.isPalindrome("TENET"));
     }
 
 }
