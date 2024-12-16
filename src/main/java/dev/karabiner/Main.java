26c4843f97c606df59138807ba82e8bdfbcf4877
Karabiner07 <joyphilip.p2001@gmail.com>
2024-12-16 20:41:41 +0530

    Reverse Array

--- src/main/java/dev/karabiner/Main.java
+++ src/main/java/dev/karabiner/Main.java
@@ -6,7 +6,7 @@ import dev.karabiner.array.ArrayUtil;
 public class Main {
     public static void main(String[] args) {
 
-        ArrayUtil.printArray(new int[] {2, 11, 5, 10, 7, 9, 32});
+        ArrayUtil.printArray(ArrayUtil.reverseArray(new int[] {2, 11, 5, 10, 7, 9, 32}));
 
     }
 
--- src/main/java/dev/karabiner/array/ArrayUtil.java
+++ src/main/java/dev/karabiner/array/ArrayUtil.java
@@ -7,4 +7,19 @@ public class ArrayUtil {
                 System.out.print(array[i] + " ");
             }
         }
+
+        public static int[] reverseArray(int[] array) {
+            int start = 0, end = array.length - 1;
+
+            while (start < end) {
+                int temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+
+                start++;
+                end--;
+            }
+
+            return array;
+        }
 }
